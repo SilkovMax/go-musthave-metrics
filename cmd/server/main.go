6fae479aa95123b2b31b2e70f40c782ca0b27639
SilkovMax <silkov34@yandex.ru>
2026-07-14 02:30:55 +0300

    Iter6: add logging with zap

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,19 +4,103 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
+
 
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 
 
 	"github.com/SilkovMax/go-musthave-metrics/internal/handler"
 	"github.com/SilkovMax/go-musthave-metrics/internal/repository"
 
-	"os"
+
 )
+
+
+var Log *zap.Logger = zap.NewNop()
+
+// Initialize инициализирует синглтон логера с необходимым уровнем логирования.
+func Initialize(level string) error {
+    // преобразуем текстовый уровень логирования в zap.AtomicLevel
+    lvl, err := zap.ParseAtomicLevel(level)
+    if err != nil {
+        return err
+    }
+    // создаём новую конфигурацию логера
+    cfg := zap.NewProductionConfig()
+    // устанавливаем уровень
+    cfg.Level = lvl
+    // создаём логер на основе конфигурации
+    zl, err := cfg.Build()
+    if err != nil {
+        return err
+    }
+    // устанавливаем синглтон
+    Log = zl
+    return nil
+}
+
+
+type responseData struct {
+	status int
+	size   int
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size //  размер ответа
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// По умолчанию 200 OK
+		resData := &responseData{
+			status: http.StatusOK,
+			size:   0,
+		}
+
+		lw := &loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   resData,
+		}
+
+		next.ServeHTTP(lw, r)
+
+		duration := time.Since(start)
+
+		Log.Info("HTTP request data",
+			zap.String("uri", r.RequestURI),
+			zap.String("method", r.Method),
+			zap.Duration("duration", duration),
+			zap.Int("status", resData.status),
+			zap.Int("size", resData.size),
+		)
+	})
+}
+
+
 
 func main() {
 	//add flag on address + port
 	address := flag.String("a", "localhost:8080", "address and port to run server")
+	logLevel := flag.String("l", "info", "log level")
 
 	flag.Parse()
 
@@ -25,9 +109,23 @@ func main() {
 		*address = envAddress
 	}
 
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		*logLevel = envLogLevel
+	}
+
+	if err := Initialize(*logLevel); err != nil {
+		panic(fmt.Errorf("failed to initialize logger: %w", err))
+	}
+
+	defer Log.Sync()
+
 	storage := repository.NewMemStorage()
 
 	r :=chi.NewRouter()
+
+	//запускаю логировангие для каждого запроса
+	r.Use(LoggingMiddleware)
+
 
 	r.Post("/update/{type}/{name}/{value}", handler.NewUpdateHandler(storage).ServeHTTP)
 
@@ -38,9 +136,9 @@ func main() {
 
 
 
-	fmt.Printf("Сервер запущен на http://%s\n", *address)
+	Log.Info("Сервер запущен", zap.String("address", *address))
 	err := http.ListenAndServe(*address, r)
 	if err != nil {
-		fmt.Printf("Ошибка запуска сервера: %v\n", err)
+		Log.Fatal("Ошибка запуска сервера", zap.Error(err))
 	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.26.1
 require (
 	github.com/go-chi/chi/v5 v5.3.0 // indirect
 	github.com/go-resty/resty/v2 v2.17.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.28.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
 )
